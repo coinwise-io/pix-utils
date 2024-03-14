d1ed3044dca96b22b45c1cf398e42ae62ff80772
cesarhro <cesarhr.olimpio@gmail.com>
2024-03-14 12:55:14 -0300

    chore: added types at the index

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ export { parsePix } from './parser';
 export { hasError, isStaticPix, isDynamicPix } from './validate';
 export { PixError } from './types/pixError';
 export * from './types/pixElements';
+export {
+  CreateDynamicPixParams,
+  CreateStaticPixParams,
+} from './types/pixCreate';
+export { PIXPaylodParams } from './types/pixDynamicPayload';
